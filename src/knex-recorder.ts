02979fa4fa908f580a4c2e47f033c7eae43b34cf
Wes Johnson <wes@swj.io>
2021-12-06 10:43:49 -0500

    do not snapshot w/ bindings by default

--- src/knex-recorder.ts
+++ src/knex-recorder.ts
@@ -15,23 +15,29 @@ interface LocalGlobal {
   sqlQueryInfo: SQLQueryInfo;
 }
 
+interface RecordOptions {
+  withBindings?: boolean;
+}
+
 const localGlobal: LocalGlobal = global as any;
 
-const knexQueryEventHandler = (knex: Knex) => (data: KnexQueryEvent) => {
-  const active = localGlobal.sqlQueryInfo.activeTest;
-  if (!active) {
-    console.warn(
-      `[jest-sql-snapshots] Skipping recording query because there's no active test running. Have you configured the jest environment correctly?`
-    );
-    return;
-  }
-  const queries = localGlobal.sqlQueryMap[active];
-  if (!queries || !localGlobal.sqlQueryInfo.running) {
-    return;
-  }
-  const sqlString = knex.raw(data.sql, data.bindings).toString();
-  queries.push(sqlString);
-};
+const knexQueryEventHandler =
+  (knex: Knex, options: RecordOptions) => (data: KnexQueryEvent) => {
+    const active = localGlobal.sqlQueryInfo.activeTest;
+    if (!active) {
+      console.warn(
+        `[jest-sql-snapshots] Skipping recording query because there's no active test running. Have you configured the jest environment correctly?`
+      );
+      return;
+    }
+    const queries = localGlobal.sqlQueryMap[active];
+    if (!queries || !localGlobal.sqlQueryInfo.running) {
+      return;
+    }
+    const bindings = options.withBindings ? data.bindings : [];
+    const sqlString = knex.raw(data.sql, bindings).toString();
+    queries.push(sqlString);
+  };
 
 let eventHandler: (...args: any[]) => void;
 
@@ -40,8 +46,8 @@ let eventHandler: (...args: any[]) => void;
  *
  * recordKnexEvents(knex);
  */
-export const recordKnexEvents = (knex: Knex) => {
-  eventHandler = knexQueryEventHandler(knex);
+export const recordKnexEvents = (knex: Knex, options: RecordOptions = {}) => {
+  eventHandler = knexQueryEventHandler(knex, options);
   knex.on("query", eventHandler);
 };
 
